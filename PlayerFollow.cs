1d403c20efba03f8413bb29ce4c8a2a18bab3c21
Juan Antonio Ordaz Cuahonte <urkidy_23@hotmail.com>
2018-08-30 12:46:43 -0500

    branch master

---
+++ PlayerFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+    private Vector3 _cameraOffset;
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    public float turnSmoothing = 15f;
+
+
+
+    // Use this for initialization
+    void Start () {
+        _cameraOffset = transform.position - PlayerTransform.position;
+	}
+	
+	// LateUpdate is called atfer update
+	void LateUpdate () {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+	}
+
+    void Rotating(float lh, float lv)
+    {
+        Vector3 targetDirection = new Vector3(lh, 0f, lv);
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+        Quaternion newRotation = Quaternion.Lerp(GetComponent<Rigidbody>().rotation, targetRotation, turnSmoothing * Time.deltaTime);
+
+        GetComponent<Rigidbody>().MoveRotation(newRotation);
+    }
+}
---
+++ control.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour 
+{
+
+    public float Speed = 5f;
+    private CharacterController _controller;
+    public Animator anim;
+    private float inputH;
+    private float inputV;
+    public Rigidbody rbody;
+    public float turnSmoothing = 15f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+
+        float moveX = inputH * 20f * Time.deltaTime;
+        float moveZ = inputV * 50f * Time.deltaTime;
+        rbody.velocity = new Vector3(moveX, 0f, moveZ);
+    }
+
+    void FixedUpdate()
+    {
+        float lh = Input.GetAxisRaw("Horizontal");
+        float lv = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void Rotating(float lh, float lv)
+    {
+        Vector3 targetDirection = new Vector3(lh, 0f, lv);
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+        Quaternion newRotation = Quaternion.Lerp(GetComponent<Rigidbody>().rotation, targetRotation, turnSmoothing * Time.deltaTime);
+       
+        GetComponent<Rigidbody>().MoveRotation(newRotation);
+    }
+
+
+}
